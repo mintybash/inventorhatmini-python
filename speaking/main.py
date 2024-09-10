2cd562edf2b27a602fc8f7a3bed042dcd5a01f8a
mintybash <128168360+mintybash@users.noreply.github.com>
2024-09-10 17:05:12 +0100

    Update main.py

--- speaking/main.py
+++ speaking/main.py
@@ -1,1 +1,29 @@
+import time
+import subprocess
+from inventorhatmini import InventorHATMini
 
+"""
+Play a WAV audio file from your Inventor HAT Mini, using Aplay!
+
+Press "User" to play the sound.
+
+Note: WAV files need to be signed 16-bit
+"""
+
+# Create a new InventorHATMini
+board = InventorHATMini(init_leds=True)
+
+print("Press the 'User' button to play the sound.")
+
+# Loop forever
+while True:
+    if board.switch_pressed():
+        # Play the sound
+        print("Playing ... ", end="")
+
+        # Use aplay to play the sound
+        command = "aplay -q ./ahoy.wav &"
+        subprocess.run(command, shell=True)
+
+        time.sleep(0.25)
+        print("Done")
